9ff97b72580b8707245821de847839347a4e422b
Kalebe Rocha <bezerra.kalebe@gmail.com>
2022-09-19 22:22:32 -0300

    Modificando tela inicial

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,6 +3,20 @@ import { View, Button, Text, Image, TouchableOpacity } from 'react-native';
 
 import { styles } from './styles';
 
+import { 
+  Inter_100Thin,
+  Inter_200ExtraLight,
+  Inter_300Light,
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+  Inter_900Black 
+} from '@expo-google-fonts/inter';
+import {useFonts} from 'expo-font';
+import AppLoading from 'expo-app-loading';
+
 import Soon from '../../assets/Soon.png'
 import { Background } from '../../components/Background';
 
@@ -11,6 +25,23 @@ interface HomeProps{
 }
 export function Home() {
 
+    // função para retornar as fonts depois do loading do app
+
+    let [fontsLoaded, error] = useFonts({
+      Inter_100Thin,
+      Inter_200ExtraLight,
+      Inter_300Light,
+      Inter_400Regular,
+      Inter_500Medium,
+      Inter_600SemiBold,
+      Inter_700Bold,
+      Inter_800ExtraBold,
+      Inter_900Black 
+    })
+  
+    if (!fontsLoaded) {
+      return <AppLoading/>;
+    }
   
   
   return (
@@ -24,7 +55,7 @@ export function Home() {
           </TouchableOpacity>
         </View>
         <View style={styles.contenText}>
-          <Text>Bem Vindo</Text>
+          <Text style={styles.titleInitial}>Bem Vindo</Text>
           <Text style={styles.subTitle}>Selecione um dia e veja as ocupações dos ambientes</Text>
         </View>
       </View>
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,32 +1,40 @@
-import { StyleSheet } from 'react-native';
-import { THEME } from '../../themes';
+import { StyleSheet } from "react-native";
+import { color } from "react-native-reanimated";
+import { THEME } from "../../themes";
 
 export const styles = StyleSheet.create({
   container: {
-    width:'100%',
-    height: '100%',
-    flex:1,
-    backgroundColor: '#EFEFEF'
+    width: "100%",
+    height: "100%",
+    backgroundColor: "#EFEFEF",
   },
-  containerLogo:{
-    width: '100%',
-    height: 'auto',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 40
-  },
-  contenText:{
-    flex:1,
-    width:'100%',
-    alignItems: 'center',
-    position: 'relative',
-    marginTop:40
-  },
-  title:{
+  containerLogo: {
+    width: "100%",
+    height: "auto",
+    paddingTop: 10,
+    paddingBottom: 0,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 40,
 
   },
-  subTitle:{
-    
-
-  }
+  contenText: {
+    flex: 1,
+    width: "100%",
+    alignItems: "center",
+    position: "relative",
+    marginTop: 40,
+  },
+  titleInitial: {
+    fontSize: 35,
+    fontFamily: 'Inter_800ExtraBold',
+    color: '#25B5E9'
+  },
+  subTitle: {
+    fontSize: 18,
+    fontFamily: 'Inter_800ExtraBold',
+    color: '#3F3C3C',
+    textAlign: "center",
+    paddingHorizontal: 10,
+  },
 });
