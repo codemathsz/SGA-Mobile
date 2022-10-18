718bacacb2a9948dae5e1cc2cf1cb5916101df74
Kalebe Rocha <bezerra.kalebe@gmail.com>
2022-10-17 22:17:41 -0300

    :lipstick: Estilização Busca Avançada

--- src/screens/AdvancedSearch/index.tsx
+++ src/screens/AdvancedSearch/index.tsx
@@ -20,81 +20,79 @@ export function AdvancedSearch() {
         subTitle="Solicite a disponibilidade de um Professor e um Ambiente"
       />
 
-      <View>
-        <ScrollView>
-          <View style={styles.containerForm}>
-            <View style={styles.divForm}>
-              <Text style={styles.titleForm}>Curso</Text>
-              <TextInput
-                style={styles.inputForm}
-                placeholder="Digite o nome do Curso"
-              />
-            </View>
-            <View style={styles.divForm}>
-              <Text style={styles.titleForm}>Competência</Text>
-              <View style={styles.selectForm}>
-                <Picker
-                  selectedValue={selectedCompetence}
-                  onValueChange={(itemValue) =>
-                    setSelectedCompetence(itemValue)
-                  }
-                  mode={"dropdown"}
-                >
-                  <Picker.Item
-                    label="Selecione a Competência"
-                    value="none"
-                    style={styles.itemSelect}
-                  />
-                </Picker>
-              </View>
-            </View>
-            <View style={styles.divForm}>
-              <Text style={styles.titleForm}>Empresa</Text>
-              <TextInput
-                style={styles.inputForm}
-                placeholder="Digite o nome da Empresa"
-              />
-            </View>
-            <View style={styles.divForm}>
-              <Text style={styles.titleForm}>Onde será o treinamento ?</Text>
-              <View style={styles.containerRadio}>
-                <View style={styles.divRadio}>
-                  <RadioButton
-                    value="senai"
-                    color="black"
-                    status={localeClasses === "senai" ? "checked" : "unchecked"}
-                    onPress={() => setLocaleClasses("senai")}
-                  />
-                  <Text>Senai</Text>
-                </View>
-                <View style={styles.divRadio}>
-                  <RadioButton
-                    value="company"
-                    color="black"
-                    status={
-                      localeClasses === "company" ? "checked" : "unchecked"
-                    }
-                    onPress={() => setLocaleClasses("company")}
-                  />
-                  <Text>Empresa</Text>
-                </View>
-              </View>
-            </View>
-            {localeClasses === "company" ? (
-              <View style={styles.divForm}>
-                <Text style={styles.titleForm}>CEP</Text>
-                <TextInput
-                  style={styles.inputForm}
-                  placeholder="Digite o cep do local da empresa"
-                />
-              </View>
-            ) : (
-              ""
-            )}
-            <View></View>
+      <ScrollView>
+        <View style={styles.containerForm}>
+          <View style={styles.divForm}>
+            <Text style={styles.titleForm}>Curso</Text>
+            <TextInput
+              style={styles.inputForm}
+              placeholder="Digite o nome do Curso"
+            />
           </View>
-        </ScrollView>
-      </View>
+          <View style={styles.divForm}>
+            <Text style={styles.titleForm}>Competência</Text>
+            <View style={styles.selectForm}>
+              <Picker
+                selectedValue={selectedCompetence}
+                onValueChange={(itemValue) => setSelectedCompetence(itemValue)}
+                mode={"dropdown"}
+              >
+                <Picker.Item
+                  label="Selecione a Competência"
+                  value="none"
+                  style={styles.itemSelect}
+                />
+              </Picker>
+            </View>
+          </View>
+          <View style={styles.divForm}>
+            <Text style={styles.titleForm}>Empresa</Text>
+            <TextInput
+              style={styles.inputForm}
+              placeholder="Digite o nome da Empresa"
+            />
+          </View>
+          <View style={styles.divForm}>
+            <Text style={styles.titleForm}>Onde será o treinamento ?</Text>
+            <View style={styles.containerRadio}>
+              <View style={styles.divRadio}>
+                <RadioButton
+                  value="senai"
+                  color="black"
+                  status={localeClasses === "senai" ? "checked" : "unchecked"}
+                  onPress={() => setLocaleClasses("senai")}
+                />
+                <Text>Senai</Text>
+              </View>
+              <View style={styles.divRadio}>
+                <RadioButton
+                  value="company"
+                  color="black"
+                  status={localeClasses === "company" ? "checked" : "unchecked"}
+                  onPress={() => setLocaleClasses("company")}
+                />
+                <Text>Empresa</Text>
+              </View>
+            </View>
+          </View>
+          {localeClasses === "company" ? (
+            <View style={styles.divForm}>
+              <Text style={styles.titleForm}>CEP</Text>
+              <TextInput
+                style={styles.inputForm}
+                placeholder="Digite o cep do local da empresa"
+              />
+            </View>
+          ) : (
+            ""
+          )}
+          {/* Fazer aqui os date */}
+          <View style={styles.divForm}></View>
+          <View style={styles.divForm}></View>
+          {/* TEste */}
+          
+        </View>
+      </ScrollView>
     </Background>
   );
 }
