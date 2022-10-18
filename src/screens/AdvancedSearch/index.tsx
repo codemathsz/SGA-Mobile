7b5c11c7e59aa385ac21e0ce31534befca31f8d9
Kalebe Rocha <bezerra.kalebe@gmail.com>
2022-10-17 21:30:51 -0300

    :lipstick: Codando Busca Avançada

--- src/screens/AdvancedSearch/index.tsx
+++ src/screens/AdvancedSearch/index.tsx
@@ -1,6 +1,7 @@
-import { Picker, PickerIOS } from "@react-native-picker/picker";
 import React, { useState } from "react";
 import { View, Text, ScrollView, TextInput } from "react-native";
+import { Picker, PickerIOS } from "@react-native-picker/picker";
+import { RadioButton } from "react-native-paper";
 import { Background } from "../../components/Background";
 import { Header } from "../../components/Header";
 
@@ -9,6 +10,8 @@ import { styles } from "./styles";
 export function AdvancedSearch() {
   // const para o select da competência
   const [selectedCompetence, setSelectedCompetence] = useState([]);
+  // const para RadioButton
+  const [localeClasses, setLocaleClasses] = useState("senai");
 
   return (
     <Background>
@@ -37,7 +40,11 @@ export function AdvancedSearch() {
                   }
                   mode={"dropdown"}
                 >
-                  <Picker.Item label="Selecione a Competência" value="none" style={styles.itemSelect}/>
+                  <Picker.Item
+                    label="Selecione a Competência"
+                    value="none"
+                    style={styles.itemSelect}
+                  />
                 </Picker>
               </View>
             </View>
@@ -48,6 +55,43 @@ export function AdvancedSearch() {
                 placeholder="Digite o nome da Empresa"
               />
             </View>
+            <View style={styles.divForm}>
+              <Text style={styles.titleForm}>Onde será o treinamento ?</Text>
+              <View style={styles.containerRadio}>
+                <View style={styles.divRadio}>
+                  <RadioButton
+                    value="senai"
+                    color="black"
+                    status={localeClasses === "senai" ? "checked" : "unchecked"}
+                    onPress={() => setLocaleClasses("senai")}
+                  />
+                  <Text>Senai</Text>
+                </View>
+                <View style={styles.divRadio}>
+                  <RadioButton
+                    value="company"
+                    color="black"
+                    status={
+                      localeClasses === "company" ? "checked" : "unchecked"
+                    }
+                    onPress={() => setLocaleClasses("company")}
+                  />
+                  <Text>Empresa</Text>
+                </View>
+              </View>
+            </View>
+            {localeClasses === "company" ? (
+              <View style={styles.divForm}>
+                <Text style={styles.titleForm}>CEP</Text>
+                <TextInput
+                  style={styles.inputForm}
+                  placeholder="Digite o cep do local da empresa"
+                />
+              </View>
+            ) : (
+              ""
+            )}
+            <View></View>
           </View>
         </ScrollView>
       </View>
